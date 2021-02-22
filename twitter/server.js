2201cc931b2fc70471c380abec19b3844df9044d
khouloud912 <60706637+khouloud912@users.noreply.github.com>
2021-02-22 16:20:12 +0100

    Update server.js

--- twitter/server.js
+++ twitter/server.js
@@ -37,7 +37,7 @@ const twitterSecretAPIKey = "Place you secret consumer key here";
 const twitterAccessToken = "Place your access token here";
 const twitterSecretAccessToken = "Place your secret access token here";
 const targetUrl = "Place your server's url here";
-const projectId = "Place your dialogflow projectId here";
+const projectId = "newagent-cfh9";
 const environmentName = "Place your twitter environment name here";
 
 const twitterOAuth = {
