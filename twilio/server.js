d42ec46e8d2e3a4a06c32d1113ed3792c69af340
SharpAdHub <80128799+SharpAdHub@users.noreply.github.com>
2021-03-08 13:52:20 +0530

    Update server.js

--- twilio/server.js
+++ twilio/server.js
@@ -29,10 +29,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //See https://dialogflow.com/docs/reference/v2-auth-setup and
 // https://cloud.google.com/dialogflow/docs/setup for details.
 
-const projectId = 'Place your dialogflow projectId here';
-const phoneNumber = "Place your twilio phone number here";
-const accountSid = 'Place your accountSid here';
-const authToken = 'Place your authToken here';
+const projectId = 'whatsapp-vefc';
+const phoneNumber = "+1 415 523 8886";
+const accountSid = 'ACb320df030e572d6999e651dde4ca864a';
+const authToken = '2aa37e77b739374892fdc21935125059';
 
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
