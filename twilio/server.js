f03e41e2e7e7312c3d393e93d39a4e0d7e4d1ced
ARJUN_HTML-DEV <85322232+USERBOT-SUPPORT@users.noreply.github.com>
2021-11-25 12:27:12 +0530

    Update server.js

--- twilio/server.js
+++ twilio/server.js
@@ -29,10 +29,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //See https://dialogflow.com/docs/reference/v2-auth-setup and
 // https://cloud.google.com/dialogflow/docs/setup for details.
 
-const projectId = 'Place your dialogflow projectId here';
-const phoneNumber = "Place your twilio phone number here";
-const accountSid = 'Place your accountSid here';
-const authToken = 'Place your authToken here';
+const projectId = 'arjun-chat-bot-ckhs';
+const phoneNumber = "+919664903431";
+const accountSid = 'ACdcfbce2cbb7f97947740eceb2120ce4d';
+const authToken = '24157545c0cf7691fd043e6ca59d3215';
 
 const client = require('twilio')(accountSid, authToken);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
